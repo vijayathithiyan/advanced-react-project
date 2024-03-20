113d031bc5cfd8c6b00b6cab5a8c7a02ba6ccd34
vijayathithiyan <144885363+vijayathithiyan@users.noreply.github.com>
2024-03-20 15:42:41 +0530

    Add files via upload

---
+++ Front-end/day 1/src/components/UI/Navbar.jsx
@@ -0,0 +1,18 @@
+import '../../assets/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Classes</li>
+            <li>Contact</li>
+            <li>SocialMedia</li>
+            
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
